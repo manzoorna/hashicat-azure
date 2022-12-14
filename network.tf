b36cd8ef999bf8f957d668aee548141ace5d5853
manzoorna <manzoor.nayeem@gmail.com>
2022-12-14 11:42:51 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/ML-Mumbai/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "manzoorna-workshop"
 }
